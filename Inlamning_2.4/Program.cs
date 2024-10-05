41b4d3786606f551b5686567d52abda32814eb95
krbu06001 <krbu06001@E-PW05CGQY>
2024-10-05 09:38:54 +0200

    Add project files.

---
+++ Inlamning_2.4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System; 
+
+namespace uppgift_2._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad är lönen för den första personen?");
+            int först = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Vad är lönen för den andra personen?");
+            int andra = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Vad är lönen för den tredje personen");
+            int tredje = int.Parse(Console.ReadLine());
+
+            int sum = först + andra + tredje;
+            int medellön = sum / 2;
+
+            Console.WriteLine("Medellönen av de tre anställda är " + medellön + " kronor.");
+        }
+        
+    }
+}
